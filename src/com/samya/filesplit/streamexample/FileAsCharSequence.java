c7ce9be73384870526163322587cce37a5a0fdc8
Ghosh <samya.ms10@gmail.com>
2019-03-21 14:52:50 +0100

    File data as Stream

---
+++ src/com/samya/filesplit/streamexample/FileAsCharSequence.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.samya.filesplit.streamexample;
+
+import java.io.*;
+import java.util.regex.Pattern;
+
+/**
+ * @author Samya Ghosh 
+ *
+ */
+
+
+public class FileAsCharSequence implements CharSequence  {
+
+	
+
+	private final int length;
+    private final StringBuilder buffer = new StringBuilder();
+    private final InputStream input;
+ 
+    public FileAsCharSequence(File file) throws FileNotFoundException {
+        if (file.length() > (long) Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("File is too long to handle as character sequence");
+        }
+        this.length = (int) file.length();
+        this.input = new FileInputStream(file);
+    }
+ 
+    @Override
+    public int length() {
+        return length;
+    }
+ 
+    @Override
+    public char charAt(int index) {
+        ensureFilled(index + 1);
+        return buffer.charAt(index);
+    }
+ 
+ 
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        ensureFilled(end + 1);
+        return buffer.subSequence(start, end);
+    }
+ 
+    private void ensureFilled(int index) {
+        if (buffer.length() < index) {
+            buffer.ensureCapacity(index);
+            final byte[] bytes = new byte[index - buffer.length()];
+            try {
+                int length = input.read(bytes);
+                if (length < bytes.length) {
+                    throw new IllegalArgumentException("File ended unexpected");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                buffer.append(new String(bytes, "utf-8"));
+            } catch (UnsupportedEncodingException ignored) {
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        Pattern p = Pattern.compile("[,\\.\\-;]");
+        final CharSequence splitIt = 
+            new FileAsCharSequence(
+                   new File("sample.txt"));
+        System.out.println("file data as stream ");
+        p.splitAsStream(splitIt).forEach(System.out::println);
+    }
+
+}
